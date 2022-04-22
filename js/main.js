8866d3f8156c553f6657bca77993d4c537939831
Jaan <jaan242@hotmail.com>
2022-04-22 12:42:07 +0300

    add similar color input and blur effect

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 // Create the canvas
 var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
+var similarInput = document.getElementById("similar-input");
 
 var COLOR_DIFF = 5;
 
 var pixels = [];
 pixels[(canvas.height * canvas.width) - 1] = 0;
+
+var neighborVectors = [
+	-canvas.width - 1, -canvas.width, -canvas.width + 1,
+	-1, 1,
+	canvas.width - 1, canvas.width, canvas.width + 1
+];
 
 var timer;
 
@@ -25,61 +31,124 @@ function shuffle(array) {
 
 function formatHex(value) {
 	var str = value.toString(16);
-	if (str.length < 6) {
+	while (str.length < 6) {
 		str = "0" + str;
 	}
 	return str;
+}
+
+function drawCanvas(canvas) {
+	var ctx = canvas.getContext("2d");
+	ctx.clearRect(0,0,canvas.width,canvas.height);
+	for (var i = 0; i < pixels.length; i++) {
+		ctx.fillStyle = "#" + formatHex(pixels[i]);
+		ctx.fillRect((i % canvas.width), Math.floor(i / canvas.width), 1, 1);
+	}
+}
+
+function splitColor(color) {
+	return [color >> 16, color >> 8 & 0xFF, color & 0xFF];
+}
+
+function isSimilarColor(color, other) {
+	var a = splitColor(color);
+	var b = splitColor(other);
+	for (var i = 0; i < 3; i++) {
+		if (Math.abs(a[i] - b[i]) >= similarInput.value) {
+			return false;
+		}
+	}
+	return true;
+}
+
+function countSimilarColor(i) {
+	var count = 0;
+	var color = pixels[i];
+	for (var j = 0; j < neighborVectors.length; j++) {
+		var neighbor = i + neighborVectors[j];
+		if (neighbor >= 0 && neighbor < pixels.length && pixels[neighbor] != -1
+		 && isSimilarColor(color, pixels[neighbor])) {
+			count++;
+		}
+	}
+	return count;
+}
+
+function updatePixelRandom(i) {
+	var color = 0;
+	var refColor = -1;
+	shuffle(neighborVectors);
+	for (var j = 0; j < neighborVectors.length; j++) {
+		var neighbor = i + neighborVectors[j];
+		if (neighbor >= 0 && neighbor < pixels.length && pixels[neighbor] != -1) {
+			refColor = pixels[neighbor];
+			break;
+		}
+	}
+	if (refColor != -1) {
+		var refR = refColor >> 16;
+		var refG = refColor >> 8 & 0xFF;
+		var refB = refColor & 0xFF;
+		var colorR = randomIntInRange(Math.max(0, refR - COLOR_DIFF), Math.min(255, refR + COLOR_DIFF));
+		var colorG = randomIntInRange(Math.max(0, refG - COLOR_DIFF), Math.min(255, refG + COLOR_DIFF));
+		var colorB = randomIntInRange(Math.max(0, refB - COLOR_DIFF), Math.min(255, refB + COLOR_DIFF));
+		color = colorR << 16 | colorG << 8 | colorB;
+	} else {
+		color = randomIntInRange(0, 0xFFFFFF);
+	}
+	pixels[i] = color;
+}
+
+function updatePixelAverage(i) {
+	var color = 0;
+	var refColor = -1;
+	var sumR = 0;
+	var sumG = 0;
+	var sumB = 0;
+	var sumCount = 0;
+	for (var j = 0; j < neighborVectors.length; j++) {
+		var neighbor = i + neighborVectors[j];
+		if (neighbor >= 0 && neighbor < pixels.length && pixels[neighbor] != -1) {
+			refColor = pixels[neighbor];
+			sumR += refColor >> 16;
+			sumG += refColor >> 8 & 0xFF;
+			sumB += refColor & 0xFF;
+			sumCount++;
+		}
+	}
+	var colorR = sumR / sumCount;
+	var colorG = sumG / sumCount;
+	var colorB = sumB / sumCount;
+	color = colorR << 16 | colorG << 8 | colorB;
+	pixels[i] = color;
 }
 
 function resetColors() {
 	for (var i = 0; i < pixels.length; i++) {
 		pixels[i] = -1;
 	}
+	for (var i = 0; i < pixels.length; i++) {
+		updatePixelRandom(i);
+	}
+	drawCanvas(canvas);
 }
 
 function drawNew() {
-	// var pixelIndexes = [];
+	var similarCountTotal = 0;
 	for (var i = 0; i < pixels.length; i++) {
-		// pixelIndexes[i] = i;
-	// }
-	//shuffle(pixelIndexes);
-	// for (var idx = 0; idx < pixelIndexes.length; idx++) {
-		// var i = pixelIndexes[idx];
-		var color = 0;
-		var refColor = -1;
-		var neighborPixels = [
-			i - canvas.width - 1, i - canvas.width, i - canvas.width + 1,
-			i - 1, i + 1,
-			i + canvas.width - 1, i + canvas.width, i + canvas.width + 1
-		];
-		shuffle(neighborPixels);
-		for (var j = 0; j < neighborPixels.length; j++) {
-			var neighbor = neighborPixels[j];
-			if (neighbor >= 0 && neighbor < pixels.length && pixels[neighbor] != -1) {
-				refColor = pixels[neighbor];
-				break;
-			}
+		var similarCount = countSimilarColor(i);
+		// if (70000 < i && i < 90000) {
+		if (similarCount < 4) {
+			updatePixelAverage(i);
+		} else if (similarCount > 4) {
+			updatePixelRandom(i);
+			similarCountTotal++;
 		}
-		if (refColor != -1) {
-			var refR = refColor >> 16;
-			var refG = refColor >> 8 & 0xFF;
-			var refB = refColor & 0xFF;
-			var colorR = randomIntInRange(Math.max(0, refR - COLOR_DIFF), Math.min(255, refR + COLOR_DIFF));
-			var colorG = randomIntInRange(Math.max(0, refG - COLOR_DIFF), Math.min(255, refG + COLOR_DIFF));
-			var colorB = randomIntInRange(Math.max(0, refB - COLOR_DIFF), Math.min(255, refB + COLOR_DIFF));
-			color = colorR << 16 | colorG << 8 | colorB;
-		} else {
-			color = randomIntInRange(0, 0xFFFFFF);
-		}
-		pixels[i] = color;
 	}
-	ctx.clearRect(0,0,canvas.width,canvas.height);
-	for (var i = 0; i < pixels.length; i++) {
-		ctx.fillStyle = "#" + formatHex(pixels[i]);
-		ctx.fillRect((i % canvas.width), Math.floor(i / canvas.width), 1, 1);
-	}
+	drawCanvas(canvas);
 	var iterSpan = document.getElementById("iter-span");
 	iterSpan.innerText = parseInt(iterSpan.innerText) + 1;
+	document.getElementById("similar-span").innerText = similarCountTotal;
 }
 
 function startTimer(element) {
@@ -94,16 +163,15 @@ function stopTimer(element) {
 	document.getElementById("start-btn").disabled = false;
 }
 
+function updateSimilarInput(element) {
+	document.getElementById("similar-input-span").innerText = element.value;
+}
+
 function onClickReset() {
 	resetColors();
 	document.getElementById("iter-span").innerText = "0";
-	drawNew();
-	var canvas = document.getElementById("origCanvas");
-	var ctx = canvas.getContext("2d");
-	for (var i = 0; i < pixels.length; i++) {
-		ctx.fillStyle = "#" + formatHex(pixels[i]);
-		ctx.fillRect((i % canvas.width), Math.floor(i / canvas.width), 1, 1);
-	}
+	//drawNew();
+	drawCanvas(document.getElementById("origCanvas"));
 }
 
 onClickReset();
